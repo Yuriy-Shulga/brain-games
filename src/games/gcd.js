7b929cab06b24d6ed9170a1f02b2ad5dde8a9e1f
Yuriy Shulga <jsshu1ga@gmail.com>
2023-12-28 20:10:08 +0300

    Add the game "brain-gcd"

---
+++ src/games/gcd.js
@@ -0,0 +1,33 @@
+import runGameEngine from '../index.js';
+import getRandomNumber from '../utilits.js';
+
+const descriptionOfGame = 'Find the greatest common divisor of given numbers.';
+
+const getGreatestCommonDivisor = (num1, num2) => {
+  if (num1 % num2 === 0) {
+    return num2;
+  }
+
+  return getGreatestCommonDivisor(num2, num1 % num2);
+};
+
+const genGameData = () => {
+  console.log('=======');
+  const num1 = getRandomNumber(1, 1000);
+  const num2 = getRandomNumber(1, 1000);
+
+  const gcd = getGreatestCommonDivisor(num1, num2);
+
+  if (gcd === 1) {
+    return genGameData();
+  }
+
+  const answer = String(gcd);
+
+  const question = `${num1} ${num2}`;
+  return [question, answer];
+};
+
+export default () => {
+  runGameEngine(genGameData, descriptionOfGame);
+};
