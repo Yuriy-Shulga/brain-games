4f7eb86501681d10b466a7b587452a19c9110d73
Yuriy Shulga <jsshu1ga@gmail.com>
2023-12-28 19:24:04 +0300

    Add the game "brain-even"

---
+++ src/games/even.js
@@ -0,0 +1,17 @@
+import runGameEngine from '../index.js';
+import getRandomNumber from '../utilits.js';
+
+const descriptionOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (num) => num % 2 === 0;
+
+const genGameData = () => {
+  const question = getRandomNumber(1, 1000);
+  const answer = isEven(question) ? 'yes' : 'no';
+
+  return [question, answer];
+};
+
+export default () => {
+  runGameEngine(genGameData, descriptionOfGame);
+};
